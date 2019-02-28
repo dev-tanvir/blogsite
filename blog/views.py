4d15d9c0c7ac230a6034921f346d3106d4e52581
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-03-01 05:32:46 +0600

    extending the post detail views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    ''' this method is for landing page of blog site '''
+    ''' this function is for landing page of blog site '''
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    ''' this function is for post details of blog site '''
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
