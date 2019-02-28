4b7e608443f4fe6b685e712374265b86bbaa8844
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-03-01 02:25:11 +0600

    added base template for extending

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
-
 # Create your views here.
 def post_list(request):
+    ''' this method is for landing page of blog site '''
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
