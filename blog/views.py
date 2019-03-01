d2aa451ade58ce93db602dfbcba303577406e150
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-03-02 04:14:13 +0600

    adding editing option

--- blog/views.py
+++ blog/views.py
@@ -19,12 +19,15 @@ def post_new(request):
     # import pdb;pdb.set_trace()
     if request.method == 'POST':
         form = PostForm(request.POST)
-        if form.is_valid:
+
+        if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+
     else:
         form = PostForm()
+
     return render(request, 'blog/post_edit.html', {'form': form})
